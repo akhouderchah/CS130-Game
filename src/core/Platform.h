4f85838946653b40a1fd2b73b39230c346eb0fe4
Alex <alex.khouderchah@gmail.com>
2016-12-24 09:44:53 -0800

    Platform-independent byte-swap utility functions for endianness

--- src/core/Platform.h
+++ src/core/Platform.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Config.h"
+#include <cstdint>
 
 enum EPlatforms
 {
@@ -24,13 +25,29 @@ enum EBuildTypes
 #endif
 
 #if(SYSTEM_TYPE == EP_WINDOWS)
-#include <Windows.h>
+#include <stdlib.h>
 #elif(SYSTEM_TYPE == EP_LINUX)
-// Linux-specific includes
+#include <endian.h>
 #elif(SYSTEM_TYPE == EP_MAC_OSX)
 // OSX-specific includes
 #else
 // Platform-indendent alternatives only
+#endif
+
+// Byte-swap for handling endianness issues
+#if COMPILER_IS_GCC && ((__GNUC__ == 4 && __GNUC_MINOR__ >= 3) || __GNUC__ >  4)
+#define bswap16 __builtin_bswap16__
+#define bswap32 __builtin_bswap32__
+#define bswap64 __builtin_bswap64__
+#elif COMPILER_IS_MSVC
+#define bswap16 _byteswap_ushort
+#define bswap32 _byteswap_ulong
+#define bswap64 _byteswap_uint64
+#else
+#define _CUSTOM_BSWAP
+uint16_t bswap16(uint16_t value);
+uint32_t bswap32(uint32_t value);
+uint64_t bswap64(uint64_t value);
 #endif
 
 // Run at the beginning of execution to do platform-specific initialization
--- src/core/_private/Platform.cpp
+++ src/core/_private/Platform.cpp
@@ -16,3 +16,30 @@ bool programInitialize()
 	return success;
 }
 
+#ifdef _CUSTOM_BSWAP
+uint16_t bswap16(uint16_t value)
+{
+	return ((value & 0xFF00) >> 8u) |
+		((value & 0x00FF) << 8u);
+}
+
+uint32_t bswap32(uint32_t value)
+{
+	return ((value & 0xFF000000u) >> 24u) |
+		((value & 0x00FF0000u) >> 8u) |
+		((value & 0x0000FF00u) << 8u) |
+		((value & 0x000000FFu) << 24u);
+}
+
+uint64_t bswap64(uint64_t value)
+{
+	return ((value & 0xFF00000000000000ull) >> 56u) |
+		((value & 0x00FF000000000000ull) >> 40u) |
+		((value & 0x0000FF0000000000ull) >> 24u) |
+		((value & 0x000000FF00000000ull) >>  8u) |
+		((value & 0x00000000FF000000ull) <<  8u) |
+		((value & 0x0000000000FF0000ull) << 24u) |
+		((value & 0x000000000000FF00ull) << 40u) |
+		((value & 0x00000000000000FFull) << 56u);
+}
+#endif //_CUSTOM_BSWAP
